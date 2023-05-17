cdd2a965c92fad91cc94af3805c7df2b2387e092
EduardoHinojosa127 <eduardo.hinojosa@tecsup.edu.pe>
2023-05-17 12:47:32 -0500

    editingStyle implementado

--- HinojosaColeccionDeJuegos/JuegosViewController.swift
+++ HinojosaColeccionDeJuegos/JuegosViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class JuegosViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    @IBOutlet weak var eliminarBoton: UIButton!
+    @IBOutlet weak var agregarActualizarBoton: UIButton!
     var imagePicker = UIImagePickerController()
-
+    var juego:Juego? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePicker.delegate = self
+        
+        if juego != nil {
+            imageView.image = UIImage(data: (juego!.imagen!) as Data)
+            tituloTextField.text = juego!.titulo
+            agregarActualizarBoton.setTitle("Actualizar", for: .normal)
+        }else{
+            eliminarBoton.isHidden = true
+        }
         // Do any additional setup after loading the view.
     }
     
@@ -38,12 +49,23 @@ class JuegosViewController: UIViewController, UIImagePickerControllerDelegate, U
     
     
     @IBAction func agregarTapped(_ sender: Any) {
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-        let juego = Juego(context: context)
-        juego.titulo = tituloTextField.text
-        juego.imagen = imageView.image?.jpegData(compressionQuality: 0.50)
+        if juego != nil {
+           juego!.titulo! = tituloTextField.text!
+           juego!.imagen = imageView.image?.jpegData(compressionQuality: 0.50)
+        }else{
+           let context = (UIApplication.shared.delegate as!
+               AppDelegate).persistentContainer.viewContext
+           let juego = Juego(context:context)
+           juego.titulo = tituloTextField.text
+           juego.imagen = imageView.image?.jpegData(compressionQuality: 0.50)
+        }
         (UIApplication.shared.delegate as! AppDelegate).saveContext()
         navigationController?.popViewController(animated: true)
     }
     
+    @IBAction func eliminarTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        context.delete(juego!)
+        navigationController?.popViewController(animated: true)
+    }
 }
--- HinojosaColeccionDeJuegos/ViewController.swift
+++ HinojosaColeccionDeJuegos/ViewController.swift
@@ -28,6 +28,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
+        tableView.isEditing = true
         // Do any additional setup after loading the view.
     }
     override func viewWillAppear(_ animated: Bool) {
@@ -39,7 +40,52 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             
         }
     }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let juego = juegos[sourceIndexPath.row]
+        juegos.remove(at: sourceIndexPath.row)
+        juegos.insert(juego, at: destinationIndexPath.row)
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        do {
+            try context.save()
+        } catch {
+            // Manejar el error en caso de fallo al guardar los cambios
+        }
+    }
 
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let juego = juegos[indexPath.row]
+        performSegue(withIdentifier: "juegoSegue", sender: juego)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let siguienteVC = segue.destination as! JuegosViewController
+        siguienteVC.juego = sender as? Juego
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let juego = juegos[indexPath.row]
+            
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            context.delete(juego)
+            
+            do {
+                try context.save()
+                juegos.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            } catch {
+                // Manejar el error en caso de fallo al guardar los cambios o eliminar el registro
+            }
+        }
+    }
+
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
 
 }
 
